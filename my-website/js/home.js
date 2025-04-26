9e5cafc9de23db36f3e0e5d337dbcc7f123f8877
noyrobles19 <noyrobles19@gmail.com>
2025-04-26 18:46:22 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '4805fee141236fd075133fd593f71e48';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
